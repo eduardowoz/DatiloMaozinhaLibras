0b95565813ee82c09e97918471bb2d463425ce3a
israelnobre15 <israel.nobre2004@gmail.com>
2025-06-23 21:13:34 -0300

    adicionando modelo 'final'

--- prediction.py
+++ prediction.py
@@ -10,7 +10,7 @@ from tensorflow import keras
 
 # --- CONFIGURAÇÕES ---
 # Caminho para o modelo treinado e para o label encoder
-MODEL_PATH = "modelo_todas_letras.keras"
+MODEL_PATH = "modelo_todas_letras (7).keras"
 LABELS_PATH = "label_encoder_classes.npy"
 SEQUENCE_LENGTH = 30
 PREDICTION_THRESHOLD = 0.7
